f1a12ceeee3501f1b37f9b009e73890a951a2709
deepak <deepakkumar74491234@gmail.com>
2022-08-14 09:48:22 +0530

    adding in questions 14 aug

---
+++ index.js
@@ -0,0 +1,25 @@
+const quiz =[
+    {
+        question : "Q1) How many wonders of world are there ? " ,
+        a : "8",
+        b : "7",
+        c : "6",
+        d : "9",
+    }
+    {
+        question : "Q2 ) What is the value of tan(90) ? " ,
+        a : "1",
+        b : "1/3",
+        c : "Infinity",
+        d : "0",
+
+    }
+    {
+        question : "Q3 ) What is the formula of simple interest  ? " ,
+        a : "(P*R*T)/365",
+        b : "(P*R*T*H)/100",
+        c : "(P*R*T)/100",
+        d : "(365*P*R*T)/100",
+
+    }
+]
