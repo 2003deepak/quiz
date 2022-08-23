8f576ebdbe7ae380f62bcbca3b33490c15183964
2003deepak <98113881+2003deepak@users.noreply.github.com>
2022-08-23 15:25:47 +0530

    change in index.js

--- index.js
+++ index.js
@@ -1,2 +1,69 @@
-var a = document.getElementById("question") ;
-a.innerHTML("HELLO EVERYONE MY NAME IS DEEPAK ")
+let easyDB = [
+    {
+
+    question: "What does HTML stand for?",
+
+    a: "Hyper Text Preprocessor",
+    b: "Hyper Text Markup Language",
+    c: "Hyper Text Multiple Language",
+    d: "Hyper Tool Multi Language",
+    ans : "ans4"
+     
+    
+  },
+    {
+        question: "What does css stand for?",
+
+    a: "css",
+    b: "Hyper Text Markup Language",
+    c: "Hyper Text Multi hple Language",
+    d: "Hyper Tool Multisg  Language",
+    ans : "ans4"
+    
+  },
+    {
+        question: "What does HTML stand for?",
+
+        a: "Hyper Text Preprocessor",
+        b: "Hyper Text Markup Language",
+        c: "Hyper Text Multiple Language",
+        d: "Hyper Tool Multi Language",
+        ans : "ans4"
+  },
+    {
+        question: "What does HTML stand for?",
+
+        a: "Hyper Text Preprocessor",
+        b: "Hyper Text Markup Language",
+        c: "Hyper Text Multiple Language",
+        d: "Hyper Tool Multi Language",
+        ans : "ans4"
+  },
+    {
+        question: "What does HTML stand for?",
+
+        a: "Hyper Text Preprocessor",
+        b: "Hyper Text Markup Language",
+        c: "Hyper Text Multiple Language",
+        d: "Hyper Tool Multi Language",
+        ans : "ans4"
+  },
+ 
+];
+
+let finalmarks = 0 ;
+
+const question = document.querySelector('.question');
+const opt1 = document.querySelector('#opt1');
+const opt2 = document.querySelector('#opt2');
+const opt3 = document.querySelector('#opt3');
+const opt4 = document.querySelector('#opt4');
+const submit = document.querySelector('#submit');
+
+
+const loadquestion = () => {
+
+    question.innerHTML(Hello);
+}
+
+loadquestion();
