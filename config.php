b2861180d685292bce536e2555cd76f5d3d6ab46
deepak <deepakkumar74491234@gmail.com>
2022-08-28 11:26:27 +0530

    changein confog

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhsot";
+$servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "kconlinequiz";
@@ -7,7 +7,8 @@ $dbname = "kconlinequiz";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
-    die("<script>alert('Connection Failed.')</script>");
+    die("<script>alert('Oops we are some problem pls try again later ')</script>");
+    echo "<script> location.replace('signup.html')</script> ";
 }
 
 ?>
