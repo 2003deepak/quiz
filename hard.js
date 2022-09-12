6b36f8ec9996cdc5444c68ba978470410803652d
2003deepak <98113881+2003deepak@users.noreply.github.com>
2022-09-12 22:50:55 +0530

    Update hard.js

--- hard.js
+++ hard.js
@@ -32,7 +32,7 @@ const quizData = [
         correct: "a",
     },
     {
-        question: "5)Which of these is not a moon of saturn ? ",
+        question: "5)Which of these is a moon of saturn ? ",
         a: "Titan",
         b: "Europa",
         c: "Moon",
@@ -259,7 +259,7 @@ submitBtn.addEventListener('click', () => {
        } else {
            let percentage = (score/50 )*100 ;
 
-           if(percentage>=50){
+           if(percentage>=60){
             quiz.innerHTML = `
               <h2>You have scored ${score} out of 50 marks </h2>
               <h2> You have secured ${percentage} %  in the exam 
